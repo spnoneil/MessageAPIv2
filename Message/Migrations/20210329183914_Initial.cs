45e2280585647009411df7885c55fe17ffb0d5de
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-29 13:40:11 -0500

    correct datatypes
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Connor Burgess <connorburgesscodes@gmail.com>

---
+++ Message/Migrations/20210329183914_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Message.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BMessages",
+                columns: table => new
+                {
+                    BMessageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Message = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: true),
+                    Posted = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BMessages", x => x.BMessageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BMessages");
+        }
+    }
+}
