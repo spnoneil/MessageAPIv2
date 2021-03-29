23a2fe9bbd7bbad711e4e96cfbfe7d776b46eae2
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-29 15:40:34 -0500

    add seeded data
    Co-authored-by: Connor Burgess <connorburgesscodes@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- Message/Models/MessageContext.cs
+++ Message/Models/MessageContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System;
 namespace Message.Models
 {
   public class MessageContext : DbContext
@@ -6,6 +7,18 @@ namespace Message.Models
     public MessageContext(DbContextOptions<MessageContext> options)
       : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<BMessage>()
+          .HasData(
+              new BMessage { BMessageId = 1, Message = "Matilda", Posted = DateTime.Now },
+              new BMessage { BMessageId = 2, Message = "Rexie", Posted = DateTime.Now },
+              new BMessage { BMessageId = 3, Message = "Matilda", Posted = DateTime.Now },
+              new BMessage { BMessageId = 4, Message = "Pip", Posted = DateTime.Now },
+              new BMessage { BMessageId = 5, Message = "Bartholomew", Posted = DateTime.Now }
+          );
     }
 
     public DbSet<BMessage> BMessages { get; set; }
