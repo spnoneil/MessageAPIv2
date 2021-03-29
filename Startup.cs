879cc8f6042305cf7fd6bc9cb6f6c04d97b86ca6
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-29 13:14:42 -0500

    add dependencies: Entity, Pomelo
    Co-authored-by: Connor Burgess <connorburgesscodes@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace MessageAPI.Solution
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MessageAPI.Solution v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
